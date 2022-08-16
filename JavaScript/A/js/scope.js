dc08529fc4da6206dd8671629ef793762bebf3a9
Naeem Odat <naeemodat@ttu.edu.jo>
2022-08-16 20:47:13 +0300

    javascript demos

--- JavaScript/A/js/scope.js
+++ JavaScript/A/js/scope.js
@@ -4,13 +4,13 @@ console.log("gloabal: message= "+msg);
 var a=function(){
 	var msg="Inside a";
 	console.log("a: message= "+msg);
-	function b(){
-		console.log("b: message= "+msg);
-	}
+	// function b(){
+	// 	console.log("b: message= "+msg);
+	// }
 	b();
 };
-// function b(){
-// 	console.log("b: message= "+msg);
-// }
+function b(){
+	console.log("b: message= "+msg);
+}
 
 a();
